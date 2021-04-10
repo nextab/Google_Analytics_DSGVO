e22620b4196ef63059db608fd55221e6ec4df040
nexTab - Oliver Gehrmann <info@nextab.de>
2021-04-10 16:09:52 +0200

    Removed non-script codes

---
+++ scripts.js
@@ -0,0 +1,9 @@
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=XX-XXXXXXXXX-X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'XX-XXXXXXXXX-X', { 'anonymize_ip': true });
+</script>
